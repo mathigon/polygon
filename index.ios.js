6742aafa13ffe976780e5b78d4ed2dbc4703c7b5
Philipp Legner <philipp@mathigon.org>
2016-10-04 08:21:14 +0100

    iOS grid and polygon data

--- index.ios.js
+++ index.ios.js
@@ -1,15 +1,41 @@
 import React, { Component } from 'react';
-import { AppRegistry, Text, TabBarIOS, StyleSheet, View } from 'react-native';
+import { AppRegistry, Image, ListView, TouchableHighlight, Text, TabBarIOS, StyleSheet, View } from 'react-native';
+
 
 class Polygon extends Component {
-  state = {
-    selectedTab: 'polygons'
-  };
+
+  constructor() {
+    super();
+    const ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
+    this.state = {
+      selectedTab: 'polygons',
+      dataSource: ds.cloneWithRows(['row 1', 'row 2', 'row 3', 'row 4', 'row 5', 'row 6', 'row 7', 'row 8'])
+    };
+  }
+
+  _renderRow(rowData: string, sectionID: number, rowID: number) {
+    var imgSource = {
+      uri: 'https://fbcdn-dragon-a.akamaihd.net/hphotos-ak-ash3/t39.1997/p128x128/851549_767334479959628_274486868_n.png'
+    };
+    return (
+        <TouchableHighlight underlayColor='rgba(0,0,0,0)'>
+          <View>
+            <View style={styles.row}>
+              <Image style={styles.thumb} source={imgSource} />
+              <Text style={styles.text}>{rowData}</Text>
+            </View>
+          </View>
+        </TouchableHighlight>
+    );
+  }
 
   _renderContent(title: string) {
     return (
       <View style={styles.tabContent}>
         <Text style={styles.tabText}>{title}</Text>
+        <ListView contentContainerStyle={styles.list}
+                  dataSource={this.state.dataSource}
+                  renderRow={this._renderRow}/>
       </View>
     );
   };
@@ -42,6 +68,32 @@ class Polygon extends Component {
 }
 
 var styles = StyleSheet.create({
+  list: {
+    justifyContent: 'center',
+    flexDirection: 'row',
+    flexWrap: 'wrap'
+  },
+  row: {
+    justifyContent: 'center',
+    padding: 5,
+    margin: 10,
+    width: 100,
+    height: 100,
+    backgroundColor: '#F6F6F6',
+    alignItems: 'center',
+    borderWidth: 1,
+    borderRadius: 5,
+    borderColor: '#CCC'
+  },
+  thumb: {
+    width: 64,
+    height: 64
+  },
+  text: {
+    flex: 1,
+    marginTop: 5,
+    fontWeight: 'bold'
+  },
   tabContent: {
     flex: 1,
     alignItems: 'center',
