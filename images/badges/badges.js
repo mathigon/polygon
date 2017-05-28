dc5f5130364020b1c55b9df9f64c157c84662d85
Philipp Legner <philipp@mathigon.org>
2017-05-28 23:06:55 +0100

    Add icons for powers and badges

---
+++ images/badges/badges.js
@@ -0,0 +1,10 @@
+export default {
+  pythagoras: require('./pythagoras.png'),
+  fermat: require('./fermat.png'),
+  fibonacci: require('./fibonacci.png'),
+  plato: require('./plato.png'),
+  euler: require('./euler.png'),
+  euclid: require('./euclid.png'),
+  gauss: require('./gauss.png'),
+  archimedes: require('./archimedes.png')
+};
