c3b5d4426596d130e1d6581c000d1b58ad115ba3
Philipp Legner <philipp@mathigon.org>
2017-05-29 18:03:04 +0100

    Add greyscale images for badges and powers

---
+++ images/powerup-bw/powerups.js
@@ -0,0 +1,14 @@
+module.exports = [
+  require('./powerup-1.png'),
+  require('./powerup-2.png'),
+  require('./powerup-3.png'),
+  require('./powerup-4.png'),
+  require('./powerup-5.png'),
+  require('./powerup-6.png'),
+  require('./powerup-7.png'),
+  require('./powerup-8.png'),
+  require('./powerup-9.png'),
+  require('./powerup-10.png'),
+  require('./powerup-11.png'),
+  require('./powerup-12.png')
+];
