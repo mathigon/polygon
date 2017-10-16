f9b2a50f6515289f27eff185ba8f0f39df66b87e
Philipp Legner <philipp@mathigon.org>
2017-10-16 21:21:36 +0200

    Add android version

---
+++ index.android.js
@@ -0,0 +1,10 @@
+// -----------------------------------------------------------------------------
+// Polygon App - Android Entry File
+// @author Philipp Legner
+// -----------------------------------------------------------------------------
+
+
+import { AppRegistry } from 'react-native';
+import { App } from './src/app.js'
+
+AppRegistry.registerComponent('Polygon', () => App);
